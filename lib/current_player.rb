3a843f68214553a8428e8d0e56aee0fbfb153828
Katleiah <katleiahramos@gmail.com>
2018-03-01 19:16:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,11 +3,10 @@ def turn_count(board)
 
   board.each do |space|
     counter = 0
-    if space == " X " || space == " O "
+    if space == "X" || space == "O"
      counter += 1
-
     end
-    return counter 
+    return counter
    end
 
 end
